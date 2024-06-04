8090a5978969b2b6f53f2d01455957f1185588a3
Juliano Ribeiro <julianorib@gmail.com>
2024-06-04 13:38:39 -0300

    updates

---
+++ exemplo-basic/src/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pagina de teste</title>
+</head>
+<body>
+    <div style="text-align: center;">
+        <br><br>
+        <?php
+        echo gethostname();
+        ?>
+    </div>
+</body>
+</html>
